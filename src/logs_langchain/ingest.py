425a43e0a118540260ba07a618133a38dc4ec52b
Shahvir Buhariwalla <shahvirb@gmail.com>
2025-05-18 15:05:17 -0500

    move ingest_files into its own file

---
+++ src/logs_langchain/ingest.py
@@ -0,0 +1,35 @@
+from chromadb.config import Settings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import hashlib
+import logging
+
+logger = logging.getLogger(__name__)
+
+def ingest_files(file_paths, vector_store):
+    docs = []
+    for file_path in file_paths:
+        with open(file_path, "r") as f:
+            logger.info(f"Ingesting file {f.name}")
+            file_content = f.read()
+            file_hash = hashlib.sha256(file_content.encode('utf-8')).hexdigest()
+
+            existing_docs = vector_store.get(where={"file_hash": file_hash}, include=["metadatas"])
+            if existing_docs and existing_docs.get("metadatas"):
+                logger.info(f"Skipping ingestion for {f.name} with hash {file_hash}")
+            else:
+                md = {
+                    "filepath": file_path,
+                    "file_hash": file_hash,
+                }
+                docs.append(Document(page_content=file_content, metadata=md))
+                logger.info(f"File {f.name} will be indexed")
+
+    if docs:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        all_splits = text_splitter.split_documents(docs)
+        doc_ids = vector_store.add_documents(documents=all_splits)
+        logger.info(f"Indexed {len(doc_ids)} documents into the vector store")
+    else:
+        logger.info("No new documents to index")
--- src/logs_langchain/lograg.py
+++ src/logs_langchain/lograg.py
@@ -2,9 +2,8 @@ from chromadb.config import Settings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from logs_langchain import factory
+from logs_langchain import factory, ingest
 import logging
-import hashlib
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +14,8 @@ def show_vector_store_statistics(vector_store):
         if metadata and "filepath" in metadata:
             fp.add(metadata["filepath"])
     logger.info(f"Found {len(fp)} unique filepaths across {len(all_docs.get('documents', []))} documents in the vector store.")
+
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
@@ -33,30 +34,6 @@ if __name__ == "__main__":
     )
     logger.info("Chroma vector store initialized")
 
-    # Index documents
-    docs = []
-    with open("temp/syslog", "r") as f:
-        logger.info(f"Ingesting file {f.name}")
-        # calculate a hash of the file to use as metadata
-        file_content = f.read()
-        file_hash = hashlib.sha256(file_content.encode('utf-8')).hexdigest()
+    ingest.ingest_files(["temp/syslog"], vector_store)
 
-        # Check if the file is already in the vector store by file_hash
-        existing_docs = vector_store.get(where={"file_hash": file_hash}, include=["metadatas"])
-        if existing_docs and existing_docs.get("metadatas"):
-            logger.info(f"Skipping ingestion for {f.name} with hash {file_hash}")
-        else:
-            md = {
-            "filepath": "temp/syslog",
-            "file_hash": file_hash,
-            }
-            docs.append(Document(page_content=file_content, metadata=md))
-            logger.info(f"File {f.name} will be indexed")
-
-    if docs:
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        all_splits = text_splitter.split_documents(docs)
-        doc_ids = vector_store.add_documents(documents=all_splits)
-        logger.info(f"Indexed {len(doc_ids)} documents into the vector store")
-    else:
-        logger.info("No new documents to index")
+    show_vector_store_statistics(vector_store)
