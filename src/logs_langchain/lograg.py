8876e0721469f03e562e5fcbd7f648d005d5050e
Shahvir Buhariwalla <shahvirb@gmail.com>
2025-05-18 14:57:19 -0500

    adding file hash logic to check if we already have ingested the file

--- src/logs_langchain/lograg.py
+++ src/logs_langchain/lograg.py
@@ -4,15 +4,26 @@ from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from logs_langchain import factory
 import logging
+import hashlib
 
 logger = logging.getLogger(__name__)
 
+def show_vector_store_statistics(vector_store):
+    all_docs = vector_store.get(include=["metadatas", "documents"])
+    fp = set()
+    for metadata in all_docs.get("metadatas", []):
+        if metadata and "filepath" in metadata:
+            fp.add(metadata["filepath"])
+    logger.info(f"Found {len(fp)} unique filepaths across {len(all_docs.get('documents', []))} documents in the vector store.")
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
 
     factory = factory.GoogleFactory()
     llm = factory.llm()
     embeddings = factory.embeddings()
+    logger.info("LLM and embeddings initialized")
+
     vector_store = Chroma(
         client_settings=Settings(anonymized_telemetry=False),
         collection_name="lograg",
@@ -20,16 +31,32 @@ if __name__ == "__main__":
         # persist_directory=None,
         persist_directory="./temp/chroma_logs_langchain",
     )
+    logger.info("Chroma vector store initialized")
 
     # Index documents
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-
     docs = []
     with open("temp/syslog", "r") as f:
-        docs.append(Document(page_content=f.read(), metadata={"source": "syslog"}))
+        logger.info(f"Ingesting file {f.name}")
+        # calculate a hash of the file to use as metadata
+        file_content = f.read()
+        file_hash = hashlib.sha256(file_content.encode('utf-8')).hexdigest()
 
-    all_splits = text_splitter.split_documents(docs)
-    doc_ids = vector_store.add_documents(documents=all_splits)
-    logger.info(f"Indexed {len(doc_ids)} documents into the vector store.")
+        # Check if the file is already in the vector store by file_hash
+        existing_docs = vector_store.get(where={"file_hash": file_hash}, include=["metadatas"])
+        if existing_docs and existing_docs.get("metadatas"):
+            logger.info(f"Skipping ingestion for {f.name} with hash {file_hash}")
+        else:
+            md = {
+            "filepath": "temp/syslog",
+            "file_hash": file_hash,
+            }
+            docs.append(Document(page_content=file_content, metadata=md))
+            logger.info(f"File {f.name} will be indexed")
 
-    # TODO what happens if we have persistence in Chroma enabled and we keep indexing the same docs?
+    if docs:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        all_splits = text_splitter.split_documents(docs)
+        doc_ids = vector_store.add_documents(documents=all_splits)
+        logger.info(f"Indexed {len(doc_ids)} documents into the vector store")
+    else:
+        logger.info("No new documents to index")
