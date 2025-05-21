c1e165d672703fff021f46bfaca2e3ce575b1126
Shahvir Buhariwalla <shahvirb@gmail.com>
2025-05-21 13:53:06 -0500

    some more WIP

--- src/logs_langchain/app.py
+++ src/logs_langchain/app.py
@@ -1,13 +1,15 @@
+from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage
+from langchain_core.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable.config import RunnableConfig
 from langgraph.graph import END, StateGraph, START
 from langgraph.graph.message import MessagesState
+from langgraph.prebuilt import ToolNode
 from logs_langchain import factory
-from typing import cast
+from typing import cast, Literal
 import chainlit as cl
-from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage
 import logging
 
 logger = logging.getLogger(__name__)
@@ -16,26 +18,58 @@ google_factory = factory.GoogleFactory()
 llm = google_factory.llm()
 
 
-def welcome(state: MessagesState):
+@tool
+def get_weather(city: Literal["nyc", "sf"]):
+    """Use this to get weather information."""
+    if city == "nyc":
+        return "It might be cloudy in nyc"
+    elif city == "sf":
+        return "It's always sunny in sf"
+    else:
+        raise AssertionError("Unknown city")
+
+
+@tool
+def gen_number() -> int:
+    """Use this to get a random number."""
+    import random
+
+    return random.randint(1, 100)
+
+
+tools = [get_weather, gen_number]
+llm = llm.bind_tools(tools)
+tool_node = ToolNode(tools=tools)
+
+# def welcome(state: MessagesState):
+#     messages = state["messages"]
+#     prompt = ChatPromptTemplate.from_messages(
+#         [
+#             ("system", "Look at the users greeting and respond back very rudely like you're Linus Torvalds. Stay brief, one sentence only."),
+#             ("user", messages[-1].content),
+#         ]
+#     )
+#     runnable = prompt | llm | StrOutputParser()
+#     response_text = runnable.invoke({})
+#     response = SystemMessage(content=response_text)
+#     # TODO should we return all the messages or just the new one?
+#     return {"messages": [response]}
+
+
+def llm_node(state: MessagesState):
     messages = state["messages"]
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", "Look at the users greeting and respond back very rudely like you're Linus Torvalds. Stay brief, one sentence only."),
-            ("user", messages[-1].content),
-        ]
-    )
-    runnable = prompt | llm | StrOutputParser()
-    response_text = runnable.invoke({})
-    response = SystemMessage(content=response_text)
-    return {"messages": [response]}
+    response = llm.invoke(messages)
+    return {"messages": messages + [response]}
 
 
 builder = StateGraph(MessagesState)
 
-builder.add_node("first", welcome)
+builder.add_node("llm", llm_node)
+builder.add_node("tools", tool_node)
 
-builder.add_edge(START, "first")
-# builder.add_edge("final", END)
+builder.add_edge(START, "llm")
+builder.add_edge("llm", "tools")
+builder.add_edge("tools", END)
 
 graph = builder.compile()
 
