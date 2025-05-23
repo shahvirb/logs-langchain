d8c3c1a85fc557d85ba2b32e7db9a4ad75e77c0a
Shahvir Buhariwalla <shahvirb@gmail.com>
2025-05-23 16:21:42 -0500

    adding user consent

--- src/logs_langchain/tools.py
+++ src/logs_langchain/tools.py
@@ -3,6 +3,14 @@ from typing import Literal
 from logs_langchain import ssh, hosts
 
 
+def get_user_consent(prompt_message):
+    consent = input(f"{prompt_message} (y/n): ").strip().lower()
+    if consent != "y":
+        print("Consent violated. Exiting.")
+        exit(0)
+    return True
+
+
 @tool
 def get_weather(city: Literal["nyc", "sf"]):
     """Use this to get weather information."""
@@ -36,12 +44,12 @@ def read_local_file(file_path: str) -> str:
 
 
 @tool
-def ping(ipaddr_or_hostname) -> bool:
+def ping(host) -> bool:
     """Use this to ping a server. It returns True if the server is reachable, False otherwise."""
     import subprocess
 
     try:
-        output = subprocess.check_output(["ping", "-c", "1", ipaddr_or_hostname])
+        output = subprocess.check_output(["ping", "-c", "1", host])
         return True
     except subprocess.CalledProcessError:
         return False
@@ -50,9 +58,18 @@ def ping(ipaddr_or_hostname) -> bool:
 @tool
 def ssh_command(host: str, command: str) -> str:
     """Use this to run a command on a remote server via SSH. It returns a string with the command output."""
+    get_user_consent(f"Do you want to run the command '{command}' on {host}?")
     host_info = hosts.HOSTS[host]
     with ssh.SSHClient(host, host_info["username"], host_info["key_file"]) as client:
         return client.run_command(command)
 
+@tool
+# let's write a tool here which returns a random character
+def random_character() -> str:
+    """Use this to get a random character."""
+    import random
+    import string
 
-all = [get_weather, gen_number, read_local_file, ping, ssh_command]
+    return random.choice(string.ascii_letters)
+
+all = [get_weather, gen_number, read_local_file, ping, ssh_command, random_character]
